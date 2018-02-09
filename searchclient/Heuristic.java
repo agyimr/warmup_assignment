fdea9cb47f7c5d84d64592b265baf9a92dd32a4b
Jakub Niedźwiedzki <jakub1niedzwiedzki@gmail.com>
2018-02-09 14:34:52 +0100

    Heuristic fixed, seems to work well in single box-goal level

--- searchclient/Heuristic.java
+++ searchclient/Heuristic.java
@@ -4,49 +4,57 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 import java.awt.Point;
-import java.util.List;
 
 import searchclient.NotImplementedException;
 
 import static java.lang.Math.abs;
 
 public abstract class Heuristic implements Comparator<Node> {
+	private ArrayList<Point> goalPosition = new ArrayList<>();
 	public Heuristic(Node initialState) {
-		// Here's a chance to pre-process the static parts of the level.
+        for(int row = 0; row < Node.MAX_ROW; ++row) {
+            for (int column = 0; column < Node.MAX_COL; ++column) {
+                 if(Node.goals[row][column] > 0) {
+                    goalPosition.add(new Point(column, row));
+                }
+            }
+        }
 	}
 
 	public int h(Node n) {
-        List<Point> boxPosition = new ArrayList<>();
-        List<Point> goalPosition = new ArrayList<>();
+		ArrayList<Point> boxPosition = new ArrayList<>();
         for(int row = 0; row < Node.MAX_ROW; ++row) {
 			for (int column = 0; column < Node.MAX_COL; ++column) {
 				if(n.boxes[row][column] > 0) {
                     boxPosition.add(new Point(column, row));
                 }
-                else if(Node.goals[row][column] > 0) {
-                    goalPosition.add(new Point(column, row));
-                }
 			}
 		}
-		int boxToGoalManhattanDistance = Node.MAX_ROW + Node.MAX_COL;
-		for ( Point currentGoal: goalPosition) {
-            for (Point currentBox : boxPosition) {
-                if(Character.toLowerCase(n.boxes[currentBox.y][currentBox.x]) == Node.goals[currentGoal.y][currentGoal.x]) {
-                    int currentManhattanDistance = abs(currentBox.x - currentGoal.x) + abs(currentBox.y - currentGoal.y);
-                    if (currentManhattanDistance < boxToGoalManhattanDistance) boxToGoalManhattanDistance = currentManhattanDistance;
-
+		int currentManhattanDistance = Node.MAX_ROW + Node.MAX_COL + 50;
+		for ( int currentGoal = 0; currentGoal < goalPosition.size(); ++currentGoal) {
+            for (int currentBox = 0; currentBox < boxPosition.size(); ++currentBox) {
+                if(Character.toLowerCase(n.boxes[boxPosition.get(currentBox).y][boxPosition.get(currentBox).x]) ==
+                                    Node.goals[goalPosition.get(currentGoal).y][goalPosition.get(currentGoal).x]) {
+                    int boxToGoalManhattanDistance = abs(boxPosition.get(currentBox).x - goalPosition.get(currentGoal).x) +
+                                                    abs(boxPosition.get(currentBox).y - goalPosition.get(currentGoal).y);
+                    int playerToBoxManhattanDistance = abs(boxPosition.get(currentBox).x - n.agentCol) +
+                                                        abs(boxPosition.get(currentBox).y - n.agentRow);
+                    int heuristic = boxToGoalManhattanDistance + playerToBoxManhattanDistance;
+                    if (currentManhattanDistance > heuristic) {
+                        currentManhattanDistance = heuristic;
+                    }
                 }
             }
         }
-        //System.err.println("Manhattan distance:" + boxToGoalManhattanDistance);
-        return boxToGoalManhattanDistance;
+        //System.err.println("Manhattan distance:" + currentManhattanDistance);
+        return currentManhattanDistance;
 	}
 
 	public abstract int f(Node n);
 
 	@Override
 	public int compare(Node n1, Node n2) {
-		return this.f(n2) - this.f(n1);
+		return this.f(n1) - this.f(n2);
 	}
 
 	public static class AStar extends Heuristic {
